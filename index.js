f8d36be3dae1763ea14037983387ee5723bad818
Sumedh Pawar <97948684+Sumedhp11@users.noreply.github.com>
2024-08-21 14:02:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ import cors from "cors";
 import { configDotenv } from "dotenv";
 import { ConnectDb } from "./src/config/dbConfig.js";
 import treesRoutes from "./src/routes/tree-routes.js";
+import adminRoutes from "./src/routes/admin-routes.js";
+import cookieParser from "cookie-parser";
 configDotenv();
 ConnectDb();
 const server = express();
 const PORT = process.env.PORT || 8080;
 server.use(cors());
 server.use(express.json());
+server.use(cookieParser());
 
 server.get("/", (_, res) => {
   res.send("Base url For Book Tree");
 });
 server.use("/api/trees", treesRoutes);
+server.use("/api/admin", adminRoutes);
 
 server.listen(PORT, () => {
   console.log(`Server Running on PORT ${PORT}`);
