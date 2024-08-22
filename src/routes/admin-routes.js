3691de9ed16b6f2459fdad8448c25ccd871b4cf7
Sumedh Pawar <97948684+Sumedhp11@users.noreply.github.com>
2024-08-23 01:43:54 +0530

    Update admin-routes.js

--- src/routes/admin-routes.js
+++ src/routes/admin-routes.js
@@ -24,6 +24,6 @@ router.get("/get-all-trees", getAllTrees);
 router.patch("/update-tree/:treeId", updateTree);
 router.get("/edit-requests", getAllEditRequests);
 router.patch("/handle-edit-requests/:id", ChangeEditRequestStatus);
-router.get("/logout", logoutAdmin);
+router.post("/logout", logoutAdmin);
 
 export default router;
